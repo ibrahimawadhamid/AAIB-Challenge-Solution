d4c0666909def6713f7a9f0dad948ac820eec160
Ibrahim Awad <ibrahim.a.hamid@gmail.com>
2021-09-19 10:41:10 +0200

    adjust endpoints trailing slashes

--- backend/routers/report.py
+++ backend/routers/report.py
@@ -5,13 +5,13 @@ from .. import crud, schemas
 router = APIRouter(tags=["report"])
 
 
-@router.get("/reports/", response_model=List[schemas.report.Report])
+@router.get("/reports", response_model=List[schemas.report.Report])
 def reports():
     """Returns all reports in the system"""
     reports = crud.report.get_reports()
     return reports
 
-@router.get("/reports/{report_id}/", response_model=schemas.report.Report)
+@router.get("/reports/{report_id}", response_model=schemas.report.Report)
 def get_report_by_id(report_id: str):
     """Returns a single report filtered by id."""
     report = crud.report.get_report_by_id(report_id=report_id)
