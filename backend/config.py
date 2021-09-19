f478ae2c2edb5eca70087bceafa544148b9cc454
Ibrahim Awad <ibrahim.a.hamid@gmail.com>
2021-09-19 10:07:47 +0200

    added basic working backend service

---
+++ backend/config.py
@@ -0,0 +1,21 @@
+from functools import lru_cache
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    name: str = "spam_protection"
+    title: str = "Spam Protection"
+    description: str = "The backend for the Spam Protection system"
+    version: str = "0.1.0"
+    swagger: str = "/docs"
+    redoc: str = "/redoc"
+
+
+@lru_cache()
+def get_settings():
+    """Return module settings global settings.
+    This method is cached through python's lru_cache decorator, so
+    only the first request creates an object, and all other requests
+    to this method returns the same object created the first time.
+    """
+    return Settings()
